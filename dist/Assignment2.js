b38aeaf418cdb6ddf6168a5fe9fdadb0085448c9
Vartika Saxena <vartikasaxena251@gmail.com>
2022-09-05 17:12:00 +0530

    done

--- dist/Assignment2.js
+++ dist/Assignment2.js
@@ -1,3 +1,13 @@
 "use strict";
 let lname = prompt("Enter your name");
 document.write(lname);
+document.body.style.backgroundImage="linear-gradient(to right,violet,pink,silver,grey)";
+document.body.style.color="green";
+        document.body.style.fontSize="70px";
+        document.body.style.textAlign="center";
+        document.body.style.fontStyle="bolder";
+        // document.body.style.textTransform="LowerCase";
+        document.body.style.textShadow="10px 10px 10px grey";
+        document.body.style.marginTop="40vh";
+        document.body.style.fontStyle="Italic"
+        document.body.style.fontWeight="25px";
